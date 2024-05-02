a7af065d11f6c102de8d1eb5492102f40412ff42
Timur <61985197+rayhight@users.noreply.github.com>
2024-05-02 13:49:26 +0300

    Console Menu

--- app/Main.hs
+++ app/Main.hs
@@ -25,16 +25,31 @@ readData filepath = do
 
 mainLoop :: IO ()
 mainLoop = do
-    putStrLn "Enter [ 1 | 2 | q ]: "
+    putStrLn "\n\
+    \ ________________________________\n\
+    \|Choose any option:              |\n\
+    \|    1.List all the Customers    |\n\
+    \|<<<<<Customer Data Analysis>>>>>|\n\
+    \|  Customer Avarage:             |\n\
+    \|      2.1 Age                   |\n\
+    \|      2.2 Balance               |\n\
+    \|      2.3 Risk (Floar)          |\n\
+    \|      2.4 Risk (Nominal/Enum)   |\n\
+    \|      2.5 Display N cusomers    |\n\
+    \|<<<<<<<<Risk Analysis>>>>>>>>>>>|\n\
+    \|    3.List High Risk Customers  |\n\
+    \|    4.Exit                      |\n\
+    \|________________________________|"
     i <- getChar
     _ <- getChar                    -- ingone '\n' whitch for some reason are reading 
     chInputFirst i
+
 chInputFirst :: Char -> IO()
-chInputFirst i 
-    | i == 'q' = return ()
-    | i == '1' = putStrLn "First Option\n" >> mainLoop
-    | i == '2' = putStrLn "Second Option\n" >> mainLoop
-    | otherwise = putStrLn "Wrong Input\n" >> mainLoop
+chInputFirst inputChar 
+    | inputChar == 'q' = return ()
+    | inputChar == '1' = putStrLn "First Option\n" >> mainLoop
+    | inputChar == '2' = putStrLn "Second Option\n" >> mainLoop
+    | otherwise        = putStrLn "Wrong Input\n" >> mainLoop
 
 
 main :: IO ()
