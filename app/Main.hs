973b98603134dd1a8722bfadb31384cbfbfb427b
tim <kasumti@gmail.com>
2024-05-04 00:41:16 +0300

    finaly works

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,15 @@
 module Main where
-import System.IO ()
-import Data.List ()
-
-data Customer = Customer {customerId :: Int, 
-                            firstName :: String, 
-                            lastName :: String, 
-                            age :: Int, 
+import Debug.Trace
+import GHC.IO.FD (openFile)
+data Customer = Customer {customerId :: Int,
+                            firstName :: String,
+                            lastName :: String,
+                            age :: Int,
                             email :: String,
-                            balance :: Float, 
-                            riskNote :: String} 
+                            balance :: Float,
+                            riskNote :: String}
                             deriving (Show, Read)
-                            
+
 type CustomerList = [Customer]
 
 mainMenu :: IO ()
@@ -68,17 +67,38 @@ chInputSecond inputChar
     | inputChar == '5' = putStrLn "Second Fifth Option\n" >> mainMenu
     | otherwise        = putStrLn "Wrong Input\n" >> subMenu
 
--- printCustomer :: Customer -> String
--- printCustomer Customer = let str 
+-- strToCustomer :: String -> Customer
+-- strToCustomer x = read ("Customer " ++ dropWhile (/= '{') x)
 
-strToCustomer :: String -> Customer
-strToCustomer str =
-    -- read ("Customer " ++ dropWhile (/= '{') str)
-    read (dropWhile (/= '{') str)
+formatId :: Int -> String
+formatId n
+    | n < 10    = "00" ++ show n
+    | n < 100   = "0" ++ show n
+    |otherwise  = show n
+
+formatStr :: String -> Int -> String
+formatStr s n
+    | ogl < fdl = s ++ replicate (fdl - ogl) ' '
+    | otherwise = s
+    where
+        ogl = length s
+        fdl = n
+
+printCustomer :: Customer ->  String
+printCustomer (Customer id fn ln ag em ba rs) =
+    "| "++
+    formatId id  ++ " | " ++
+    formatStr (fn ++ " " ++ ln) (length  "Customer Full Name ") ++ " | " ++
+    show ag ++ " | " ++
+    formatStr em 23 ++ " | " ++
+    formatStr (show ba) (length "Customer Balance ") ++ " | " ++
+    formatStr rs (length "Risk Note ") ++ " |"
+
 
 main :: IO ()
 main = do
-    strLines <- readFile "CustomerData.txt"
-    let customeList = map strToCustomer (lines strLines)
-    print customeList
-    -- mainMenu
+
+    str <- readFile "CustomerData.txt"
+    let customersList = read str :: [Customer]
+    mapM_ (putStrLn . printCustomer) customersList
+    mainMenu
